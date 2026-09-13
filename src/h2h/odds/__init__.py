a231be7a86e1c80fe2a0f31a287f31b2fabf8e87
Filip1994 <110042732+Filip1994@users.noreply.github.com>
2026-09-14 01:02:13 +0200

    feat: export API-Football quote adapter

--- src/h2h/odds/__init__.py
+++ src/h2h/odds/__init__.py
@@ -1,9 +1,11 @@
 """Odds acquisition, normalization and validation."""
 
+from .api_football_adapter import ApiFootballQuoteAdapter
 from .provider_adapter import NormalizingProviderQuoteAdapter, ProviderQuoteAdapter
 from .snapshot_builder import build_market_snapshot
 
 __all__ = [
+    "ApiFootballQuoteAdapter",
     "NormalizingProviderQuoteAdapter",
     "ProviderQuoteAdapter",
     "build_market_snapshot",
