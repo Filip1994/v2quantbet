e1b36d38b61cd588187eea4fb473a3fbe28d2a42
Filip1994 <110042732+Filip1994@users.noreply.github.com>
2026-09-14 01:16:43 +0200

    feat: export API-Football odds ingestion

--- src/h2h/odds/__init__.py
+++ src/h2h/odds/__init__.py
@@ -1,7 +1,10 @@
 """Odds acquisition, normalization and validation."""
 
 from .api_football_adapter import ApiFootballQuoteAdapter
-from .api_football_ingestion import iter_api_football_quote_payloads
+from .api_football_ingestion import (
+    ingest_api_football_odds,
+    iter_api_football_quote_payloads,
+)
 from .provider_adapter import NormalizingProviderQuoteAdapter, ProviderQuoteAdapter
 from .snapshot_builder import build_market_snapshot
 
@@ -10,5 +13,6 @@ __all__ = [
     "NormalizingProviderQuoteAdapter",
     "ProviderQuoteAdapter",
     "build_market_snapshot",
+    "ingest_api_football_odds",
     "iter_api_football_quote_payloads",
 ]
