f44d05db1bf2d1739e009cfd4f13ff5cab681bc4
Filip1994 <110042732+Filip1994@users.noreply.github.com>
2026-09-14 18:35:47 -0400

    Export quote polling job

--- src/h2h/workers/__init__.py
+++ src/h2h/workers/__init__.py
@@ -1,5 +1,6 @@
 """Scheduled/background application jobs."""
 
+from .quote_polling import QuotePollingJob, QuoteSource
 from .runtime import WorkerRuntime, run_worker
 
-__all__ = ["WorkerRuntime", "run_worker"]
+__all__ = ["QuotePollingJob", "QuoteSource", "WorkerRuntime", "run_worker"]
