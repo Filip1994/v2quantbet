29a7c214d85da9118fc52b064f552069bb47f43a
Filip1994 <110042732+Filip1994@users.noreply.github.com>
2026-09-14 18:43:10 -0400

    Export history-aware polling job

--- src/h2h/workers/__init__.py
+++ src/h2h/workers/__init__.py
@@ -1,6 +1,14 @@
 """Scheduled/background application jobs."""
 
+from .history_quote_polling import HistoricalQuoteSource, HistoryQuotePollingJob
 from .quote_polling import QuotePollingJob, QuoteSource
 from .runtime import WorkerRuntime, run_worker
 
-__all__ = ["QuotePollingJob", "QuoteSource", "WorkerRuntime", "run_worker"]
+__all__ = [
+    "HistoricalQuoteSource",
+    "HistoryQuotePollingJob",
+    "QuotePollingJob",
+    "QuoteSource",
+    "WorkerRuntime",
+    "run_worker",
+]
